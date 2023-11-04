1154b92090231b2a6414daad4afe64bd9e18bd55
carlosjesus <cjesus@nativaconsulting.com>
2023-11-04 03:23:34 -0500

    subiendo implementación de alta de cuentas

--- src/daoImpl/CuentaDaoImpl.java
+++ src/daoImpl/CuentaDaoImpl.java
@@ -70,6 +70,135 @@ public class CuentaDaoImpl implements ICuenta {
 			
 			return guardado;
 		}
+		
+		
+		@Override
+		public boolean altaCuenta(int idUsuario, int idTipoCta, int cbu, float saldo, String detalle, int tipoMovimiento) {
+			Connection connection = null;
+			conexion= new conexion();
+			boolean isCreate = false;
+			
+			try {
+				
+				if(validarTotalCuentas(idUsuario) <= 3) {
+					
+					connection = conexion.Open();
+					String query = "INSERT INTO bd_tpint_grupo_6_lab4.cuenta (IdUsuario_Cta, FechaCreacion_Cta, IdTipoCuenta_Cta, CBU_Cta, Saldo_Cta) VALUES (?, ?, ?, ?, ?)";
+					
+					Date fechaCreacionDate = new Date();
+					java.sql.Date sqlDate = new java.sql.Date(fechaCreacionDate.getTime());
+					// Crear una declaración preparada con la consulta SQL
+					PreparedStatement preparedStatement = connection.prepareStatement(query);
+					
+					// Establecer los valores para las columnas utilizando métodos setXXX 
+					preparedStatement.setInt(1, idUsuario); 
+					preparedStatement.setDate(2, sqlDate); 
+					preparedStatement.setInt(3, idTipoCta);
+					preparedStatement.setInt(4, cbu);
+					preparedStatement.setFloat(5, saldo);
+					
+					// Ejecutar la declaración de inserción
+					preparedStatement.executeUpdate();
+					
+					logMovimientos(cbu,detalle, saldo, tipoMovimiento);
+					isCreate = true;
+				}
+				else {
+					isCreate = false;
+				}
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				conexion.close();
+			}
+			return isCreate;
+		}
+	
 
+		@Override
+		public int validarTotalCuentas(int idUsuario) {
+			conexion= new conexion();
+			String query= "SELECT count(IdUsuario_Cta) FROM bd_tpint_grupo_6_lab4.cuenta where IdUsuario_Cta = " + "'" + idUsuario +"'";
+			int count = 0;
+			try {
+				conexion.Open();
+				ResultSet rs= conexion.query(query);
+				
+				while(rs.next()) {
+					count = rs.getInt(1);
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				conexion.close();
+			}
+			return count;
+		}
+		
+		public int obtenerNumCuenta(int cbu) {
+			conexion= new conexion();
+			String query= "SELECT NumCuenta_Cta FROM bd_tpint_grupo_6_lab4.cuenta where CBU_Cta = " + "'" + cbu +"'";
+			int id = 0;
+			try {
+				conexion.Open();
+				ResultSet rs= conexion.query(query);
+				
+				while(rs.next()) {
+					id = rs.getInt(1);
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				conexion.close();
+			}
+			
+			return id;
+		}
+		
+			
+
+		@Override
+		public void logMovimientos(int cbu, String descripcion, float importe, int tipoMovimiento) {
+			Connection connection = null;
+			conexion= new conexion();
+			
+			try {
+				
+				connection = conexion.Open();
+				String query = "INSERT INTO bd_tpint_grupo_6_lab4.movimientos (NumCuenta_M, FechaMovimiento_M, Detalle_M, Importe_M, IdTipoMovimiento_M, Estado_M) VALUES (?, ?, ?, ?, ?, ?)";
+				
+				Date fechaCreacionDate = new Date();
+				java.sql.Date sqlDate = new java.sql.Date(fechaCreacionDate.getTime());
+				int numCuenta = obtenerNumCuenta(cbu);
+				String estado = "Completado" ;
+				// Crear una declaración preparada con la consulta SQL
+				PreparedStatement preparedStatement = connection.prepareStatement(query);
+				
+				// Establecer los valores para las columnas utilizando métodos setXXX
+				preparedStatement.setInt(1, numCuenta); 
+				preparedStatement.setDate(2, sqlDate); 
+				preparedStatement.setString(3, descripcion); 
+				preparedStatement.setFloat(4, importe);
+				preparedStatement.setInt(5, tipoMovimiento);
+				preparedStatement.setString(6, estado);
+				// Ejecutar la declaración de inserción
+				preparedStatement.executeUpdate();
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				conexion.close();
+			}
+			
+		}
 
 }
--- src/negocio/ICuentaNegocio.java
+++ src/negocio/ICuentaNegocio.java
@@ -6,15 +6,13 @@ import entidades.Cuenta;
 
 public interface ICuentaNegocio {
 	public List<Cuenta> ListarCuentas();
-<<<<<<< HEAD
 	
 	public boolean altaCuenta(int idUsuario, int idTipoCta, int cbu, float saldo, String detalle, int tipoMovimiento);
 	
 	public int validarTotalCuentas(int idUsuario);
 	
 	public void logMovimientos(int cbu, String descripcion, float importe, int tipoMovimiento);
-=======
 
 	boolean eliminarCuenta(int id);
->>>>>>> f63fe2a4c960f7da03ea64f015a981aaf0cb238e
+
 }
