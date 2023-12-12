267e3c7c6d112aee3f5cf3eb5f0a2f3ec57fac11
Nahuelcapo2001 <nahuel.sandovalcapo@gmail.com>
2023-12-12 02:07:33 -0300

    pequeño fix bug en movimientos

--- src/daoImpl/MovimientosDAOImpl.java
+++ src/daoImpl/MovimientosDAOImpl.java
@@ -55,7 +55,7 @@ public class MovimientosDAOImpl implements IMovimientos{
 	public List<Movimientos> getMovimientosPorCuenta(int codMovimiento) {
 		List<Movimientos> Movs = new ArrayList<Movimientos>();
 		conexion= new conexion();
-		String query= "SELECT m.*, tp.Descripcion_TM as Descripcion FROM movimientos m join tipomovimientos tp on tp.IdTipoMovimiento_TM = m.IdTipoMovimiento_M  WHERE m.NumCuentaDestino_Mo =" + codMovimiento;
+		String query= "SELECT m.*, tp.Descripcion_TM as Descripcion FROM movimientos m join tipomovimientos tp on tp.IdTipoMovimiento_TM = m.IdTipoMovimiento_M  WHERE m.NumCuentaDestino_Mo = '" + codMovimiento + "' or m.NumCuenta_M = '" + codMovimiento + "'";
 
 		try {
 			conexion.Open();
